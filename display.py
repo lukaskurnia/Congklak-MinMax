a0fa29211d3f870e761426fdcb9ce5228bcda079
eginatakasan <13517030@std.stei.itb.ac.id>
2019-10-01 17:33:17 +0700

    migrate hole lbls to kivy file and added txt label manipulator

--- display.py
+++ display.py
@@ -9,47 +9,31 @@ from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.factory import Factory
 
-class MyCongklakDisplay(BoxLayout):
+class MyCongklakDisplay(FloatLayout):
+    seed_y = [-0.15, 0.15]
+    seed_x = [0.3,0.2,0.1,0,-0.1,-0.2,-0.3]
+
     def start(self, mode):
         self.remove_start_screen()
 
         if (mode == "MvP"):
-            text = ("Bot Minimax vs Player")
+            _text = ("Bot Minimax vs Player")
         elif (mode == "RvP"):
-            text = ("Bot Random vs Player")
+            _text = ("Bot Random vs Player")
         else: #mode=="MvR"
-            text = ("Bot Minimax vs Bot Random")
+            _text = ("Bot Minimax vs Bot Random")
         
-        self.add_board(text)
+        self.boardLay.modeLbl.text = _text
     
     def remove_start_screen(self):
         self.remove_widget(self.startLay)
+        self.remove_widget(self.backImg)
 
-    def add_board(self, _text):
-        self.boardLay = FloatLayout()
-        self.add_widget(self.boardLay)
+    def set_hole_lbl(self, hole_id, number):
+        self.ids.get(self.get_lbl_id(hole_id)).text = str(number)
 
-        self.boardLay.add_widget(Label(text=_text, font_size= 50, pos_hint={"y":0.4}, ))
-
-        self.boardLay.add_widget( Image(source="Images/board.png") )
-        self.boardLay.add_widget(Label(id="7", text="0", font_size= 50, pos_hint={"x":-0.42}))
-        self.boardLay.add_widget(Label(id="15",text="0", font_size= 50, pos_hint={"x":0.42}))
-
-        self.boardLay.add_widget(Label(id="0", text="6", font_size= 50, pos_hint={"x":0.3, "y":-0.15}))
-        self.boardLay.add_widget(Label(id="1", text="6", font_size= 50, pos_hint={"x":0.2, "y":-0.15}))
-        self.boardLay.add_widget(Label(id="2",text="6", font_size= 50, pos_hint={"x":0.1, "y":-0.15}))
-        self.boardLay.add_widget(Label(id="3",text="6", font_size= 50, pos_hint={"x":0, "y":-0.15}))
-        self.boardLay.add_widget(Label(id="4",text="6", font_size= 50, pos_hint={"x":-0.1, "y":-0.15}))
-        self.boardLay.add_widget(Label(id="5",text="6", font_size= 50, pos_hint={"x":-0.2, "y":-0.15}))
-        self.boardLay.add_widget(Label(id="6",text="6", font_size= 50, pos_hint={"x":-0.3, "y":-0.15}))
-        
-        self.boardLay.add_widget(Label(id="8",text="6", font_size= 50, pos_hint={"x":0.3, "y":0.15}))
-        self.boardLay.add_widget(Label(id="9",text="6", font_size= 50, pos_hint={"x":0.2, "y":0.15}))
-        self.boardLay.add_widget(Label(id="10",text="6", font_size= 50, pos_hint={"x":0.1, "y":0.15}))
-        self.boardLay.add_widget(Label(id="11",text="6", font_size= 50, pos_hint={"x":0, "y":0.15}))
-        self.boardLay.add_widget(Label(id="12",text="6", font_size= 50, pos_hint={"x":-0.1, "y":0.15}))
-        self.boardLay.add_widget(Label(id="13",text="6", font_size= 50, pos_hint={"x":-0.2, "y":0.15}))
-        self.boardLay.add_widget(Label(id="14",text="6", font_size= 50, pos_hint={"x":-0.3, "y":0.15}))
+    def get_lbl_id(self, hole_id):       
+        return "_" + str(hole_id)
 
 class CongklakApp(App):
     def build(self):
